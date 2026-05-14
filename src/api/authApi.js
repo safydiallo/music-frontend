7c41886b08d1c7ee427aa9f00b5eb007563367b2
Aimerou DIA <diaaymerou34@gmail.com>
2026-05-14 01:05:08 +0000

    changement avec un projet avec vite

--- src/api/authApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:8080/api' });
-
-export const register = (data) => API.post('/auth/register', data);
-export const login = (data) => API.post('/auth/login', data);
